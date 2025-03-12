bc3473926e10df73d36d78c610c47829e254dd87
takahashi yumi <ym.lana0217@gmail.com>
2025-03-12 13:41:00 +0900

    リモートリポジトリの変更

---
+++ src/database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Contact::factory()->count(35)->create();
+    }
+}
